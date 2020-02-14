bf64670a2ae036d4ea31859672f22405aa3389dd
hobe6699@sina.com <hobe.1.2>
2020-02-14 21:22:53 +0800

    mac 0213

--- contract/views/handler.py
+++ contract/views/handler.py
@@ -36,16 +36,27 @@ class ContractUserHandler(StartHandler):
             lab = "<label class='label badge-danger'>否</label>"
         return mark_safe(lab)
 
+    def display_show_sing(self, obj=None, is_header=None, *args, **kwargs):
+        if is_header:
+            return '签名'
+        sig = Signature.objects.filter(name_id=obj.pk)
+        if sig:
+            signature_img = Signature.objects.get(name_id=obj.pk)
+            lab = "<img src='%s' style='width:95px; height:80px'>" % signature_img.signature
+        else:
+            lab = "<label class='label badge-danger'>None</label>"
+        return mark_safe(lab)
+
     def sings_number(self):
         person_number = ContractUser.objects.all().count()
         person_id_list = ContractUser.objects.all().values_list('id')
         sing_number = Signature.objects.filter(name_id__in=person_id_list).count()
 
         return [{"person_number": person_number, "sing_number": sing_number,
-                "no_sing_number": person_number - sing_number}]
+                 "no_sing_number": person_number - sing_number}]
 
     extra_data = sings_number
-    list_display = ['username', 'code', 'department', 'phone', display_is_sing]
+    list_display = ['username', 'code', 'department', 'phone', display_is_sing, display_show_sing]
     list_template = 'contract/list.html'
     has_del_btn = False
     has_search = True
